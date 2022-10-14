f4b0ac071c25a5f33426684a0381c1ce56e921c3
FDKevin <fdkevinzero@gmail.com>
2022-10-14 21:47:08 +0800

    fix: bump mysql to 8.0.31

--- resources/mysql/resources_darwin.go
+++ resources/mysql/resources_darwin.go
@@ -5,11 +5,11 @@ package mysql
 
 import "embed"
 
-// f1943053b12428e4c0e4ed309a636fd0 is the md5 hash.
-//go:generate ./fetch_mysql.sh mysql-8.0.28-macos11-arm64.tar.gz f1943053b12428e4c0e4ed309a636fd0
+// 63aa98fa02e84d2d0520af0fabb0435a is the md5 hash.
+//go:generate ./fetch_mysql.sh mysql-8.0.31-macos12-arm64.tar.gz 63aa98fa02e84d2d0520af0fabb0435a
 
 // To use this package in testing, download the MySQL binary first:
 // go generate -tags mysql ./...
 //
-//go:embed mysql-8.0.28-macos11-arm64.tar.gz
+//go:embed mysql-8.0.31-macos12-arm64.tar.gz
 var resources embed.FS
